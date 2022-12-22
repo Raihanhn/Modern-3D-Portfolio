a383d98a904439d7a99ea25d03eade59ebeb9aa1
raihanhn <raihanhn20@gmail.com>
2022-12-22 20:26:01 +0600

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-20 p-4 fixed z-[100] font-neue'>
+        <div className="flex items-center justify-between w-full h-full px-2 text-3xl">
+            <span>Raihan</span>
+            <div>
+                <ul className="hidden md:flex">
+                    <Link href='/about'>
+                        <li className='ml-10 text-3xl uppercase hover:border-b hover:border-[#ff1722] cursor-pointer'>
+                            About
+                        </li>
+                    </Link>
+                    <Link href='/skill'>
+                        <li className='ml-10 text-3xl uppercase hover:border-b hover:border-[#ff1722] cursor-pointer'>
+                            Skills
+                        </li>
+                    </Link>
+                    <Link href='/about'>
+                        <li className='ml-10 text-3xl uppercase hover:border-b hover:border-[#ff1722] cursor-pointer'>
+                            Projects
+                        </li>
+                    </Link>
+                    <Link href='/about'>
+                        <li className='ml-10 text-3xl uppercase hover:border-b hover:border-[#ff1722] cursor-pointer'>
+                            Contact
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    letterSpacing: {
+      tightest: "-.075em",
+    tighter: "-.05em",
+    tight: "-.025em",
+    normal: "0",
+    wide: ".025em",
+    wider: ".05em",
+    widest: ".1em",
+    widest: ".25em",
+    },
+    extend: {
+      fontFamily: {
+        neue: ["Bebas Neue", "sans-serif"],
+        kaushan: ["Kaushan Script", "cursive"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
